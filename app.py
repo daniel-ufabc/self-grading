c566e298f46fd8dfdd223a5c164d89a9bba5d8ee
Daniel M. Martin <daniel.martin@ufabc.edu.br>
2019-04-21 09:45:23 -0300

    requirements

--- app.py
+++ app.py
@@ -2,14 +2,16 @@
 
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+import inquiry 
+import re
 import pickle
 
 
 def create_app():
-  app = Flask(__name__)
-  Bootstrap(app)
+	app = Flask(__name__)
+	Bootstrap(app)
 
-  return app
+	return app
 
 app = create_app()
 
@@ -19,10 +21,28 @@ def index():
     return render_template('start.html')
 
 
+@app.route('/start/<iid>')
+def start(iid):
+	"""
+	From request, obtain:
+	* RA
+	* CÓDIGO (da prova do aluno)
+	Carrega o script de 
+	"""
+	
+	i = inquiry.load(iid)
+	
+	# discover what the next inquiry is
+    # discover what is the next test-code is
+    # render template
+
+	
 @app.route('/next')
 def next_inquiry():
+    # save answers 
     # discover what the next inquiry is
-    # discover what is the next test-code 
+    # discover what is the next test-code is
+    # render template
     
 
 
